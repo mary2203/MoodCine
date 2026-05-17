59a4e1153c60f2758d76811eeb7675738625a26a
mary <96360207+mary2203@users.noreply.github.com>
2026-05-16 22:11:03 -0600

    deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('css/moodcine.css') }}?v=2">
+    <link rel="stylesheet" href="/css/moodcine.css?v=railway3">
 
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=Source+Sans+Pro:wght@300;400;600;700&display=swap" rel="stylesheet">
 </head>
