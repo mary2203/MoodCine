2bf55524114081733bf9a0a0bda69813d9256e79
mary <96360207+mary2203@users.noreply.github.com>
2026-05-16 22:13:30 -0600

    deploy

--- resources/views/moodcine.blade.php
+++ resources/views/moodcine.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('css/moodcine.css') }}">
+    <link rel="stylesheet" href="/css/moodcine.css?v=railway3">
 </head>
 <body class="moodcine-page">
 
