b1b384b3c2b5f10b0fa13c1126f33c975821abc7
mary <96360207+mary2203@users.noreply.github.com>
2026-05-16 22:28:39 -0600

    deploy

--- resources/views/moodcine.blade.php
+++ resources/views/moodcine.blade.php
@@ -154,7 +154,6 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 </body>
 </html>
